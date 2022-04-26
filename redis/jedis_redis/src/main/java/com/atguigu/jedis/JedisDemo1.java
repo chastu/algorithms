58e06d0566a50be1414656b58a6d0953daaced3c
chacha <chacha@qq.cpm>
2022-04-26 19:58:55 +0800

    Initial commit

---
+++ redis/jedis_redis/src/main/java/com/atguigu/jedis/JedisDemo1.java
@@ -0,0 +1,45 @@
+package com.atguigu.jedis;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Set;
+
+public class JedisDemo1 {
+    public static void main(String[] args) {
+        //创建jedis对象
+        Jedis jedis = new Jedis("192.168.235.100",6379);
+        //测试
+        String value = jedis.ping();
+        System.out.println(value);
+    }
+    @Test
+    public void demo1(){
+        //创建Jedis对象
+        Jedis jedis = new Jedis("192.168.235.100",6379);
+        //添加key
+        jedis.set("k1","v1");
+        jedis.set("k2","v2");
+        jedis.set("k3","v3");
+        //得到所有key
+        Set<String> keys = jedis.keys("*");
+        //便利所有的key
+        for (String key : keys) {
+            System.out.println(key);
+        }
+    }
+    @Test
+    public void demo2(){
+        //创建Jedis对象
+        Jedis jedis = new Jedis("192.168.235.100",6379);
+        //添加
+        jedis.lpush("key1","lucy","mary","jack");
+        //取值
+        List<String> values = jedis.lrange("key1", 0, -1);
+        //遍历输出
+        for (String value : values) {
+            System.out.println(value);
+        }
+    }
+}
---
+++ testgit/src/main/java/com/atguigu/git/Tst/hello.java
@@ -0,0 +1,4 @@
+package com.atguigu.git.Tst;
+
+public class hello {
+}
