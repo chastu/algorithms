6f4a19dbf09d9c5da1da1e217ca2f69b3f3919bf
chacha <chacha@qq.cpm>
2022-04-28 16:55:25 +0800

    20220428 commit

---
+++ Data structures and algorithms/src/com/atgugu/algorithms/SleepSort.java
@@ -0,0 +1,36 @@
+package com.atgugu.algorithms;
+
+/**
+ * 天才排序法之 睡眠排序
+ */
+public class SleepSort {
+    public static void main(String[] args) {
+        int[] ints = {1, 4, 7, 3, 8, 9, 2, 6, 5};
+        SortThread[] sortThreads = new SortThread[ints.length];
+        for (int i = 0; i < sortThreads.length; i++) {
+            sortThreads[i] = new SortThread(ints[i]);
+        }
+        for (int i = 0; i < sortThreads.length; i++) {
+            sortThreads[i].start();
+        }
+    }
+}
+
+class SortThread extends Thread {
+    int ms = 0;
+
+    public SortThread(int ms) {
+        this.ms = ms;
+    }
+
+    public void run() {
+        try {
+            sleep(ms * 10 + 10);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(ms);
+    }
+}
+
